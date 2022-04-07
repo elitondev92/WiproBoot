e100707c34b08ea78e12e0f911fdfbe5681db60f
Eliton <eliton.dev@outlook.com>
2022-04-07 18:06:53 -0300

    finalização da nota fiscal execicio carrinho de compra

--- src/Exercicios/StoreWipro.java
+++ src/Exercicios/StoreWipro.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import javax.print.attribute.standard.PrinterName;
+/*
+ * Ainda faltam algumas melhorias
+ * 1º Validações de input do usuário
+ * 2º Um método para formatar os valores monetários
+ * */
 public class StoreWipro {
 	public static void main(String[] args) {
 		String [] pNomes = {"Feijão", "Arroz", "Leite"};
@@ -46,24 +52,61 @@ public class StoreWipro {
 		}
 		System.out.println();
 		System.out.println("Vlr total do carrinho: " + vlrTotalCarrinho+"\n");
-		System.out.println("Imposto de 9% cobrado no valor total da compra: "+(vlrTotalCarrinho * 0.09)+"\n");
 		System.out.println("\nOpções de pagamento\n");
-		System.out.println("[1]À vista em dinheiro ou cartão MASTERCARD, 20% de desconto.");
-		System.out.println("[2]À vista no cartão de crédito, 15% de desconto.");
-		System.out.println("[3]Em 2x, preço normal de etiqueta sem juros.");
-		System.out.println("[4]Em 3x, preço normal de etiqueta mais juros de 10%.\n");
+		System.out.println("[1]À vista (dinheiro, pix ou cartão MasterdCard) com 20% de desconto;.");
+		System.out.println("[2]À vista no crédito com 10% de desconto.");
+		System.out.println("[3]Parcelado em até 3x sem tem desconto;\n.");
 		System.out.println("Qual seria a forma de pagamento?");
+		
+		int opcaoPagamento = input.nextInt();
+		float desconto = 0f;
+		float total = 0f, tributo = 0f;
+		
+		switch (opcaoPagamento) {
+			case 1:
+				desconto = vlrTotalCarrinho * 0.2f;
+				total = vlrTotalCarrinho - desconto;
+				break;
+			case 2:
+				desconto = vlrTotalCarrinho * 0.1f;
+				total = vlrTotalCarrinho - desconto;
+				break;
+			case 3:
+				desconto = 0;
+				total = vlrTotalCarrinho;
+				break;
+			case 4:
+				
+				break;
+		}
+		printNotaFiscal(pNomes, pPreco, carrinhoIndex, carrinhoQtde, desconto, total);
 		input.close();
 	}
 
 	private static void printEstoque(String[] pNomes, int[] pEstoque, float[] pPreco) {
-		System.out.println("=================================== WIPRO STORE =======================");
+		System.out.println("=================================== WIPRO STORE ===================================");
 		System.out.printf("%5s %20s %20s %20s", "COD", "PRODUTO", "ESTOQUE", "PREÇO");
 		System.out.println();
 		for (int i=0;i < pNomes.length;i++) {
 			System.out.printf("%5s %20s %20s %20s\n", i+1, pNomes[i], pEstoque[i], pPreco[i]);
 		}
-		System.out.println("=======================================================================");
+		System.out.println("===================================================================================");
+		
+	}
+	
+	private static void printNotaFiscal(String[] pNomes, float[] pPreco, List<Integer> carrinhoIndex, List<Integer> carrinhoQtde, float desconto, float valorTotal) {
+		System.out.println("=================================== Nota Fiscal ===================================");
+		System.out.printf("%20s %20s %20s %20s", "PRODUTO", "QTDE", "PREÇO UNT.", "VALOR TOTAL");
+		System.out.println();
+		int index;
+		for (int i=0;i < carrinhoIndex.size();i++) {
+			index = carrinhoIndex.get(i);
+			System.out.printf("%20s %20s %20s %20s\n", pNomes[index], carrinhoQtde.get(i), pPreco[index], (carrinhoQtde.get(i) * pPreco[index]));
+		}
+		System.out.println("==================================================================================");
+		System.out.println("Desconto na compra: " + desconto);
+		System.out.println("Valor total a ser pago: " + valorTotal);
+		System.out.println("Imposto sobre a compra: " + (valorTotal * 0.09f));
 		
 	}
 }
