a751a6bd766519551b9171119de5195b9ed04992
Eliton <eliton.dev@outlook.com>
2022-04-07 19:02:03 -0300

    Exercicio para calcular a média de um aluno e informar se foi aprovado

---
+++ src/Exercicios/Media.java
@@ -0,0 +1,28 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Media {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite o nome do aluno: ");
+        String nomeAluno = entrada.nextLine();
+        System.out.print("Digite a primeira nota: ");
+        Integer nota1 = entrada.nextInt();
+        System.out.print("Digite a segunda nota: ");
+        Integer nota2 = entrada.nextInt();
+
+        Integer media = (nota1 + nota2) / 2;
+
+        if (media >= 6) {
+            System.out.println("O aluno " + nomeAluno + " foi aprovado!!");
+        }else {
+            System.out.println("O aluno " + nomeAluno + " foi reprovado!!");
+        }
+
+        System.out.println("média: " + media);
+	}
+
+}
